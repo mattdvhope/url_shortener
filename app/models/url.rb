8c0f683986782f43e27cd1c92c6fcdbbdd613b0d
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 15:54:52 -0500

    error with stringify

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,11 +1,11 @@
 class Url < ActiveRecord::Base
-  validates :url_location, presence: true
+  # validates :url_location, presence: true
 
   before_save :shorten_url
 
   def shorten_url
-    if self.short_url.nil?
-      self.short_url = rand(1..5)
+    if self.short_url == nil
+      self.short_url = rand('a..z')
     end
   end
 
